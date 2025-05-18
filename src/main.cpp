ead80cba2a18bec57295ed2b9acb0a32b9c5293f
Rocket <2000emirhan@gmail.com>
2025-05-18 17:33:11 +0200

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include "compiler/lexer/lex.h"
+#include "compiler/parser/parser.h"
+#include "compiler/parser/token_source.h"
+
+#include <iostream>
 
 int main() {
+    auto lexer = Lexer::for_string("int main(){int a = 5; int b = a + 2; return b;}");
+    TokenSource source(lexer);
+    Parser parser(source);
+    parser.parse_program();
     return 0;
 }
