52b29c21b833fed2d07238b312c9540677db98b0
Rocket <2000emirhan@gmail.com>
2025-05-19 14:59:02 +0200

    fix range checking for decimal integers

--- src/compiler/parser/ast/expression_tree.cpp
+++ src/compiler/parser/ast/expression_tree.cpp
@@ -1,6 +1,7 @@
 #include "expression_tree.h"
 #include "compiler/span.h"
 #include "compiler/parser/parse_exception.h"
+#include <limits>
 
 Span BinaryOperationTree::span() const {
   return _lhs->span().merge(_rhs->span());
@@ -65,7 +66,8 @@ std::optional<long> LiteralTree::parse_dec(int end) const {
       std::string sub = _value.substr(0, end);
       long l = std::stol(sub, nullptr, _base);
 
-      if (l < 0 || static_cast<unsigned long>(l) > static_cast<unsigned long>(INT_MIN)) {
+      if (l < std::numeric_limits<int>::min() ||
+          l > std::numeric_limits<int>::max()) {
           return std::nullopt;
       }
       return l;
@@ -80,7 +82,7 @@ std::optional<long> LiteralTree::parse_dec(int end) const {
 std::optional<long> LiteralTree::parse_hex(int end) const {
   try {
     std::string sub = _value.substr(2, end - 2);
-    unsigned int result = std::stoul(sub, nullptr, 16);
+    unsigned int result = std::stoul(sub, nullptr, 16); //TODO: Perform check here too?
     return static_cast<long>(result);
   } catch (const std::invalid_argument& e) {
       return std::nullopt;
